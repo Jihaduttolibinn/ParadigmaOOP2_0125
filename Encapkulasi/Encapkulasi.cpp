7fbcd00c790840021088f6aa1e0579219f46f04b
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-05-27 10:20:14 +0700

    memasukkan int main

--- Encapkulasi/Encapkulasi.cpp
+++ Encapkulasi/Encapkulasi.cpp
@@ -7,7 +7,8 @@ private:
 		saklarNo[10];
 public:
 	void setSaklarNo(int i, string value) {
-		setSaklarNo[i] = value;
+	
+			saklarNo[i] = value;
 	}
 	string getSaklarNo(int i) {
 		return saklarNo[i];
@@ -15,5 +16,20 @@ public:
 
 };
 
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+	lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.getSaklarNo(0) << endl;
+	cout << lampuRumah.getSaklarNo(1) << endl;
+	cout << lampuRumah.getSaklarNo(2) << endl;
+	cout << lampuRumah.getSaklarNo(3) << endl;
+
+	return 0;
+}
 
 
